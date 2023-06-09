cf9bd80d59dc5cd286961e926c9b650519b951b1
fray23 <mars6358@gmail.com>
2023-06-10 00:06:02 +0600

    fix

--- sync.py
+++ sync.py
@@ -1,7 +1,19 @@
 import os
 import shutil
+import hashlib
+
+def get_file_hash(file_path):
+    hash_md5 = hashlib.md5()
+    with open(file_path, 'rb') as file:
+        for chunk in iter(lambda: file.read(4096), b''):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
 
 def copy_files(source_dir, destination_dir, ignore_list):
+    if not os.path.exists(destination_dir):
+        os.makedirs(destination_dir)
+
     for item in os.listdir(source_dir):
         source_item = os.path.join(source_dir, item)
         destination_item = os.path.join(destination_dir, item)
@@ -9,28 +21,24 @@ def copy_files(source_dir, destination_dir, ignore_list):
         if item in ignore_list:
             continue
 
-        print('copy ', source_dir, destination_dir)
-
         if os.path.isfile(source_item):
-            if not os.path.exists(destination_item):
-                # Копируем файл, если он не существует в папке назначения
-                shutil.copy2(source_item, destination_item)
+            source_hash = get_file_hash(source_item)
+
+            if os.path.exists(destination_item):
+                destination_hash = get_file_hash(destination_item)
+                if source_hash == destination_hash:
+                    continue
+
+            print('start copy', source_item, destination_item)
+            shutil.copy2(source_item, destination_item)
         elif os.path.isdir(source_item):
-            if not os.path.exists(destination_item):
-                # Создаем директорию, если она не существует в папке назначения
-                shutil.copytree(source_item, destination_item, symlinks=True, ignore=shutil.ignore_patterns(*ignore_list))
+            copy_files(source_item, destination_item, ignore_list)
 
 ignore_list = [
-    'env', 
-    'bet', 
-    'venv', 
-    'test_env'
+    'env',
 ]
 
 for source_folder, destination_folder  in [
-    # ('Desktop/a79', 'Desktop/a79'),
-    # ('Desktop/qute', 'Desktop/qute'),
-    # ('Desktop/work', 'Desktop/work')
-    ('Documents', 'Documents')
+    ('', ''),
 ]:
-    copy_files('/home/fs/' + source_folder, '/run/media/fs/0bda989c-4a6b-4c9a-a4a2-93cf1100eaa8/' + destination_folder, ignore_list)
+    copy_files(source_folder, destination_folder, ignore_list)
